d2d645e30a5acaa5e732b2b2d8c33d7b36e59c0d
Sanya Gupta <sanyagup07@gmail.com>
2025-07-08 21:09:24 -0400

    updated exercise.ts and exercise-1

--- typescript-exercises/exercise.ts
+++ typescript-exercises/exercise.ts
@@ -1,6 +1,29 @@
 // Create your interfaces and enum below:
+interface Task {
+    id: number;
+    title: string;
+    description: string;
+    dueDate: Date;
+}
+
+enum Priority {
+    LOW = 'low',
+    MEDIUM = 'medium',
+    HIGH = 'high'
+}
+
+interface TaskWithPriority extends Task {
+    priority: Priority;
+}
 
 // Create your task below:
+const task: TaskWithPriority = {
+    id: 1,
+    title: 'Complete TypeScript Exercise',
+    description: 'Implement interfaces and enums in TypeScript.',
+    dueDate: new Date('2025-7-6'),
+    priority: Priority.HIGH
+};
 
 // ONCE DONE, UNCOMMENT THE LINE BELOW AND RUN THE FILE
-// console.log(task);
+console.log(task);
